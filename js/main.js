f8a53f2445ac416459ac51016f166b9d6280eb14
alymokhtar <alimokhtar23@gmail.com>
2022-01-01 20:59:13 +0200

    edit

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ let icon = document.getElementById("moon");
 icon.onclick = function () {
   document.body.classList.toggle("dark-theme");
   if (document.body.classList.contains("dark-theme")) {
-    icon.src = "/Images/sun.png";
+    icon.src = "./Images/sun.png";
   } else {
     icon.src = "/Images/moon.png";
   }
