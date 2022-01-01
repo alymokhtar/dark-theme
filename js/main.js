1a9814ffacd3212226fc0be4378dea68ea1fb34e
alymokhtar <alimokhtar23@gmail.com>
2022-01-01 19:43:15 +0200

    dark-theme-project

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let icon = document.getElementById("moon");
+
+icon.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    icon.src = "/Images/sun.png";
+  } else {
+    icon.src = "/Images/moon.png";
+  }
+};
